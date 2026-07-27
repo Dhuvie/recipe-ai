523932b3cd49ac6fb12fe480a1d65f8ec3c2c395
dhuvie <dhruvdexbajaj@gmail.com>
2026-07-27 22:28:29 +0530

    add LoadingState component

---
+++ client/src/components/LoadingState.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function LoadingState() {
+  return (
+    <div className="loading-state">
+      <div className="skeleton title-skeleton"></div>
+      <div className="skeleton text-skeleton"></div>
+      <div className="skeleton text-skeleton short"></div>
+      
+      <div className="skeleton-grid">
+        <div>
+          <div className="skeleton subtitle-skeleton"></div>
+          <div className="skeleton list-item-skeleton"></div>
+          <div className="skeleton list-item-skeleton"></div>
+          <div className="skeleton list-item-skeleton"></div>
+        </div>
+        <div>
+          <div className="skeleton subtitle-skeleton"></div>
+          <div className="skeleton list-item-skeleton"></div>
+          <div className="skeleton list-item-skeleton"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
